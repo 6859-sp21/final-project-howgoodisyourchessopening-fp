2386765cf741edc6cfc970c601c11bdf1db46bdf
Justin Lim <justinlimkz@gmail.com>
2021-05-18 11:23:31 -0400

    Update opening database

--- datafiles/clean_openingDatabase.py
+++ datafiles/clean_openingDatabase.py
@@ -1,7 +1,15 @@
 import pandas as pd
 data = pd.read_csv("openingDatabase.tsv", sep="\t")
 digits = "0123456789"
+seen = set()
+indices_to_take = []
 for index, row in data.iterrows():
+    name = row['Name']
+    if name in seen:
+        continue
+    else:
+        seen.add(name)
+        indices_to_take.append(index)
     s = row['Opening Moves']
     s_clean = ""
     for i in range(len(s)):
@@ -9,4 +17,6 @@ for index, row in data.iterrows():
         if i == 0 or (i < len(s)-1 and s[i-1] == " " and s[i+1] == " " and s[i] in digits):
             s_clean += "."
     data.loc[index]['Opening Moves'] = s_clean
-data.to_csv("openingDatabaseClean.tsv", sep='\t')
+    if data.loc[index]['Name'] in ['Sicilian', 'English']:
+        data.loc[index]['Name'] += ' Opening'
+data.iloc[indices_to_take].to_csv("openingDatabaseClean.tsv", sep='\t')
